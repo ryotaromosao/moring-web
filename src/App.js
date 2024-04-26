1693133d1bd28eba4f6d17102b6abe6ab9ba8da8
ryotaromosao <ryotaro00000726@gmail.com>
2024-04-27 08:31:14 +0900

    banner bug fixed

--- src/App.js
+++ src/App.js
@@ -16,17 +16,19 @@ function App() {
       <Navbar />
       <div className="text-overlay-parent"><TopPage /></div>
      
+      <div style={{ marginBottom: "3%" }}></div>
+
       <center><b className="murasaki">#Mission</b></center>
 
 
       <div className="scroll-text-parent">
-        <ScrollText text="午前中を有意義に過ごし、いつまでも成長し続ける人材に" />
+        <ScrollText text="有意義な朝活を通して、いつまでも成長し続ける人材に" />
       </div>
 
 
       <center><b className="murasaki">#Board Members</b></center>
      
-      <div style={{ marginBottom: "100px" }}></div>
+      <div style={{ marginBottom: "3%" }}></div>
 
 
     <Introduce
@@ -34,10 +36,11 @@ function App() {
       description={"中田幼稚園中退後、家族でUSに渡り、Jarvis Preschool、 W.H Robinson Elementary(dropout)でキャリアを積む。仙台二高・東北大学工学部を卒業後、'24に東北大学情報科学研究科へ入学。MORING AWARD Ph1では過去No.1の成績を残し、現在は菅沼研朝活部長を務める。"}
       imageUrl={'./bucho2.JPG'}
       />
+
     <Introduce
       name="Ryotaro Matsui / Founder"
       description={"香川県立高松高校・東北大学工学部を卒業後、'23年に東北大学情報科学研究科に入学。'24年に所属する菅沼・水木研究室で朝活部・給食部を國見と共に設立し、現在はSugatakuChildren.IncのCEOを務める。  好きな言葉は「人生は困難な方に進むべき。」   "}
-      imageUrl={'./sosetsu-2.jpg'}
+      imageUrl={'./matsui.jpg'}
       />
 
 
@@ -47,11 +50,11 @@ function App() {
       imageUrl={'./firstmember.JPG'}
       />
     
-    <div style={{ marginBottom: "100px" }}></div>
+    <div style={{ marginBottom: "3%" }}></div>
 
     <center><b className="murasaki">#Rules</b></center>
 
-    <div style={{ marginBottom: "100px" }}></div>
+    <div style={{ marginBottom: "4%" }}></div>
 
     <Rules
       text="・朝活宣言を行った日の朝9時までに研究室に入室する"
@@ -65,11 +68,11 @@ function App() {
       text="・シーズン王者には、額縁・朝活資金運営権が与えられる"
     />
 
-    <div style={{ marginBottom: "100px" }}></div>
+    <div style={{ marginBottom: "5%" }}></div>
 
     <center><b className="murasaki">#Members</b></center>
    
-    <div style={{ marginBottom: "100px" }}></div>
+    <div style={{ marginBottom: "3%" }}></div>
 
     <Member 
       picture1="./takahata-2.JPG"
@@ -105,11 +108,14 @@ function App() {
       name2={"TKD / dealer"}
       name3={"Ryoya Koyama"}
       />
-    <div style={{ marginBottom: "100px" }}></div>
+
+
+    <div style={{ marginBottom: "8%" }}></div>
 
 
     <center><b className="murasaki">#News</b></center>
-    <div style={{ marginBottom: "100px" }}></div>
+
+    <div style={{ marginBottom: "3%" }}></div>
 
 
     <News
@@ -121,7 +127,7 @@ function App() {
 
     <News
       name={"4/2 B3向け部活動紹介を行いました"}        
-      description={"幣部部長の千葉が学部3年生向けに部活動紹介を行いました。朝活のルールやこれまでの実績について簡単に説明しながら、朝活の魅力を熱く語りました。ペナルティ制度にはあえて言及せず、新入生の入部を図りました。"}
+      description={"弊部部長の千葉が学部3年生向けに部活動紹介を行いました。部活のルールやカルチャー、実績について簡単に説明しながら、朝活の魅力を熱く語りました。ペナルティ制度にはあえて言及しないことで、新入生の入部を図りました。"}
       imageUrl={'./B3.JPG'}
     />
 
@@ -133,7 +139,7 @@ function App() {
     />
 
 
-    <div style={{ marginBottom: "100px" }}></div>
+    <div style={{ marginBottom: "3%" }}></div>
 
 
 
--- src/components/TopPage/TopPage.jsx
+++ src/components/TopPage/TopPage.jsx
@@ -26,7 +26,7 @@ function TopPage() {
         onLoad={() => setAnimationClass('fade-in')}
       />
       <div className="text-overlay">
-        <p>MEANINGFUL&nbsp;MORING CHANGES YOUR LIFE.</p>
+        <p>MEANINGFUL MORING CHANGES&nbsp;YOUR LIFE.</p>
       </div>
     </div>
   );
